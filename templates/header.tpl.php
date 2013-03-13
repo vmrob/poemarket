b8c8c2cc7264ff269933c30265909bee4e35fc5f
ccbrown <ccbrown112@gmail.com>
2013-03-12 22:10:34 -0700

    bootstrap!

---
+++ templates/header.tpl.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<script src="js/jquery.min.js"></script>
+</head>
+<body>
--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,1 +1,7 @@
-hello world!
+<?= Template::Render('header') ?>
+
+<div class="container">
+	<h1 class="hello">Hello, world!</h1>
+</div>
+
+<?= Template::Render('footer') ?>
