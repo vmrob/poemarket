e47984e9f037431a421e3df87a532316c405332d
ccbrown <ccbrown112@gmail.com>
2013-03-12 23:07:35 -0700

    minor updater update

--- public/update.php
+++ public/update.php
@@ -2,7 +2,6 @@
 require_once dirname(dirname(__FILE__)).'/support/startup.inc.php';
 
 if (isset($_GET['secret']) && $_GET['secret'] == $_->config['git_secret']) {
-	$dir = dirname(dirname(__FILE__));
 	exec('(cd ../ && git checkout production && git pull origin production) &> /dev/null &');
 }
 ?>
