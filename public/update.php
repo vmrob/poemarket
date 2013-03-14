881a651e29d43c05a8747c7536b6aeddff107234
ccbrown <ccbrown112@gmail.com>
2013-03-14 13:03:23 -0700

    comment

--- public/update.php
+++ public/update.php
@@ -2,7 +2,8 @@
 require_once dirname(dirname(__FILE__)).'/support/startup.inc.php';
 
 if ($_->config['update_secret'] && isset($_GET['secret']) && $_GET['secret'] == $_->config['update_secret']) {
-	// TODO: make updates seamless and more robust
+	// TODO: make updates seamless and more robust (there may be some race conditions that could 
+	// yield unexpected behavior if a user is mid-script throughout the lock and update)
 	ignore_user_abort(true);
 	set_time_limit(0);
 	$lock_html = 'Eek! You caught us doing something embarrassing! The site\'s being updated, but it\'ll be done soon. Sorry you had to see us this way.';
