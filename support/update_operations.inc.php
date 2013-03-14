ae2420bc790a466dc8d3d1a2038ecb50c1b2b1a7
ccbrown <ccbrown112@gmail.com>
2013-03-14 12:35:21 -0700

    add user table

--- support/update_operations.inc.php
+++ support/update_operations.inc.php
@@ -5,5 +5,17 @@
 	
 	The site is locked down while the operations are being performed so keep them quick if possible.
 */
-return array();
+return array(
+	function($app) {
+		$app->SQL->query('
+			CREATE TABLE `poem_users` (
+				`user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`user_email` int(11) DEFAULT NULL,
+				`user_poe_session` varchar(60) DEFAULT NULL,
+				`user_secret` varchar(60) DEFAULT NULL,
+				PRIMARY KEY (`user_id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+		');
+	}
+);
 ?>
