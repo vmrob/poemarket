ccc0b8ed17f25f950fb1f4729339623fd0e8ce20
ccbrown <ccbrown112@gmail.com>
2013-03-14 11:59:05 -0700

    more update stuff

--- public/update.php
+++ public/update.php
@@ -4,13 +4,18 @@ require_once dirname(dirname(__FILE__)).'/support/startup.inc.php';
 if ($_->config['update_secret'] && isset($_GET['secret']) && $_GET['secret'] == $_->config['update_secret']) {
 	ignore_user_abort(true);
 	set_time_limit(0);
-	$lock_html = 'Eek! You caught us doing something embarrassing! The site\'s being updated, but it\'ll probably be done by the time you finish reading this. Sorry you had to see us this way.';
+	$lock_html = 'Eek! You caught us doing something embarrassing! The site\'s being updated, but it\'ll be done soon. Sorry you had to see us this way.';
+	echo exec("whoami");
 	if (try_site_lock($lock_html)) {
 		$old_ops = require(dirname(dirname(__FILE__)).'/support/update_operations.inc.php');
 		exec($_->config['update_command']);
-		$new_ops = require(dirname(dirname(__FILE__)).'/support/update_operations.inc.php');
-		for ($i = count($old_ops); $i < count($new_ops); ++$i) {
-			$new_ops[$i]($_);
+		$new_config = require(dirname(dirname(__FILE__)).'/support/config.inc.php');
+		if ($new_config['site_revision'] != $_->config['site_revision']) {
+			// yep, revision definitely changed. check for update ops
+			$new_ops = require(dirname(dirname(__FILE__)).'/support/update_operations.inc.php');
+			for ($i = count($old_ops); $i < count($new_ops); ++$i) {
+				$new_ops[$i]($_);
+			}
 		}
 		site_unlock();
 	}
