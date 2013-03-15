72ded8f8517a0e8117f0f9726aa4f1302602232b
Victor Robertson <victor.robertson.iv@gmail.com>
2013-03-15 01:57:26 -0500

    started market pages

---
+++ templates/market.tpl.php
@@ -0,0 +1,76 @@
+<script src="js/holder.js"></script>
+<div class="container">
+	<div class="row">
+		<div class="tabbable tabs-left span2">
+			<ul class="nav nav-tabs">
+				<li class="active"><a href="#currency-tab" data-toggle="tab">Currency</a></li>
+				<li><a href="#gems-tab" data-toggle="tab">Gems</a></li>
+				<li><a href="#items-tab" data-toggle="tab">Items</a></li>
+			</ul>
+		</div>
+		<div class="tab-content span10">
+			<div class="tab-pane active row" id="currency-tab">
+				<div class="span6">
+					<ul class="thumbnails">
+						<?php
+						for ($i = 0; $i < 23; ++$i) {
+							?>
+							<li><a href="#" class="thumbnail">
+								<img data-src="holder.js/50x50" alt="test image">
+							</a></li>
+							<?php
+						}
+						?>
+					</ul>
+				</div>
+				<div class="span4">
+					<div class="well well-large">
+						<h3>Orb of Alchemy</h3>
+						<table class="table">
+							<tr><td>Current Price:</td><td>0.12 GCP</td></tr>
+							<tr><td>Today:</td><td>0.15 GCP</td></tr>
+							<tr><td>Last Week:</td><td>0.15 GCP</td></tr>
+						</table>
+						<div class="text-center">
+							<button class="btn btn-large half-width-btn">Buy</button>
+							<button class="btn btn-large half-width-btn">Sell</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="tab-pane row" id="gems-tab">
+				<div class="span6">
+					<table class="table">
+						<?php
+						for ($i = 0; $i < 23; ++$i) {
+							?>
+							<tr>
+								<td><img data-src="holder.js/20x20" class="gem-table" alt="skill gem"><a href="#">Skill Gem <?= $i ?></a></td>
+								<td><img data-src="holder.js/20x20" class="gem-table" alt="skill gem"><a href="#">Skill Gem <?= $i ?></a></td>
+								<td><img data-src="holder.js/20x20" class="gem-table" alt="skill gem"><a href="#">Skill Gem <?= $i ?></a></td>
+							</tr>
+							<?php
+						}
+						?>
+					</table>
+				</div>
+				<div class="span4">
+					<div class="well well-large">
+						<h3>Freezing Pulse</h3>
+						<table class="table">
+							<tr><td>Current Price:</td><td>0.12 GCP</td></tr>
+							<tr><td>Today:</td><td>0.15 GCP</td></tr>
+							<tr><td>Last Week:</td><td>0.15 GCP</td></tr>
+						</table>
+						<div class="text-center">
+							<button class="btn btn-large half-width-btn">Buy</button>
+							<button class="btn btn-large half-width-btn">Sell</button>
+						</div>
+					</div>
+				</div>			</div>
+			<div class="tab-pane" id="items-tab">
+				<p>items tab</p>
+			</div>
+		</div>
+	</div>
+</div>
