de5fb6178c6608458817e2f170829cd4a2573691
ccbrown <ccbrown112@gmail.com>
2013-03-17 20:04:54 -0700

    added inventory update javascript

--- templates/inventory.tpl.php
+++ templates/inventory.tpl.php
@@ -1,11 +1,58 @@
 <script>
+	var initial_queue_position = 1;
+
+	function update_inventory_update_status(data) {
+		if (data.status == 'waiting') {
+			$('#inventory-update-status').text(data.status_text + ' (Position in queue: ' + data.queue_position + ')');
+			var percentage = 10 + (80 - 80 * (data.queue_position / initial_queue_position));
+			$('#inventory-update-progress').css('width', percentage + '%');
+		} else if (data.status == 'assigned') {
+			$('#inventory-update-status').text(data.status_text);
+			$('#inventory-update-progress').css('width', '90%');
+		} else if (data.status == 'failed') {
+			$('#inventory-update-status').text(data.status_text);
+			$('#inventory-update-progress').css('width', '0%');
+			$('#inventory-update-button').removeAttr('disabled');
+		} else if (data.status == 'completed') {
+			$('#inventory-update-status').text('Update complete! Refresh the page to see it.');
+			$('#inventory-update-progress').css('width', '100%');
+		}
+
+		if (data.status == 'waiting' || data.status == 'assigned') {
+			setTimeout(function() {
+				$.get('ajax/inventoryupdate.php', function(response, status, request) {
+					if (status != 'success') {
+						$('#inventory-update-status').text('An error occurred while checking on your inventory update. It should still happen, but we can\'t show its progress anymore.');
+						$('#inventory-update-progress').css('width', '0%');
+						$('#inventory-update-button').removeAttr('disabled');
+					} else {
+						update_inventory_update_status(response);
+					}
+				}, 'json');
+			}, 1500);
+		}
+	}
+
 	$(function() {
 		$('#inventory-update-button').removeAttr('disabled');
 		
 		$('#inventory-update-button').click(function() {
 			$(this).attr('disabled', 'disabled');
 			
-			$('#inventory-update-container').show();
+			$('#inventory-update-status').text('Initiating update...');
+			$('#inventory-update-progress').css('width', '5%');
+			$('#inventory-update-container').slideDown();
+			
+			$.get('ajax/inventoryupdate.php?initiate', { initiate: 1 }, function(response, status, request) {
+				if (status != 'success') {
+					$('#inventory-update-status').text('Failed to initiate update. Please try again later.');
+					$('#inventory-update-progress').css('width', '0%');
+					$('#inventory-update-button').removeAttr('disabled');
+				} else {
+					initial_queue_position = response.queue_position;
+					update_inventory_update_status(response);
+				}
+			}, 'json');
 		});
 	});
 </script>
@@ -18,9 +65,9 @@
 	</div>
 
 	<div id="inventory-update-container" class="well" style="display: none;">
-		<p class="lead" id="inventory-update-status">Initializing update...</p>
+		<p class="lead" id="inventory-update-status"></p>
 		<div class="progress progress-striped active">
-			<div class="bar" style="width: 5%;"></div>
+			<div class="bar" id="inventory-update-progress" style="width: 0%;"></div>
 		</div>
 	</div>
 
